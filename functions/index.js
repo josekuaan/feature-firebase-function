bcfa1a08bc7d0de06810365c20e24acf638fdb3e
Ukachukwu ifanyi <joshuaifeanyi67@gmail.com>
2019-10-29 13:40:42 -0700

    fixing cors

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,55 @@
-const functions = require('firebase-functions');
+
 const uuidv5 = require('uuidv5');
-const cors = require('cors')({ origin: true });
+const express = require('express');
+const cors = require('cors');
+// const cors = require('cors')({ origin: true });
+
+const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-
-
 admin.initializeApp();
-const database = admin.database().ref('/users');
+// const database = admin.database().ref('/users');
+
+
+const app = express();
+app.use(cors({ origin: '*' }));
+
+// app.use('/users', usersRoute);
+
+// export const api = functions.https.onRequest(app);
 
 
 
+app.post("/", (req, res) => {
+    const user = req.body;
 
+    return admin
+        .database()
+        .ref("/users")
+        .push(user)
+        .then(() => {
+            return res.status(200).send(user);
+        })
+        .catch(error => {
+            console.error(error);
+            return res.status(500).send("Oh no! Error: " + error);
+        });
+});
    
+exports.addUser = functions.https.onRequest(app)
 
-exports.addUser = functions.https.onRequest((req, res) => {
-    return cors(req, res, () => {
-        if (req.method !== 'POST') {
-            return res.status(401).json({
-                message: 'Not allowed'
-            })
-        }
+// exports.addUser = functions.https.onRequest((req, res) => {
+//     return cors(req, res, () => {
+//         if (req.method !== 'POST') {
+//             return res.status(401).json({
+//                 message: 'Not allowed'
+//             })
+//         }
      
-        const user = req.body
-        database.push( user );
+//         const user = req.body
+//         database.push( user );
        
-    })
-})
+//     })
+// })
 
 exports.updateUuid = functions.database.ref('/users/{usersId}/user')
     .onCreate((snapshot, context) => {
